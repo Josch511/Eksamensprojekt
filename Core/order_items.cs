793dacc771eb930cb84e0c554d2a1e3b45f11bed
Emmadelic <skytte30@gmail.com>
2026-05-05 10:36:41 +0200

    order_items tilføjet i core

---
+++ Core/order_items.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class order_items
+    {
+        public int _id { get; set; }
+        public string serial { get; set; }
+        public string image_url { get; set; }
+        public string name { get; set; }
+        public DateTime purchased_at { get; set; }
+        public int UserCustomer_id { get; set; }
+    }
+}

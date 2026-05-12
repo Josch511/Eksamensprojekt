a90c1c61532deb43d266b654eadd979ac05893cf
Jonathan Schmidt <josch511@icloud.com>
2026-05-12 12:15:18 +0200

    changed dep and type to ID and added css

--- Core/Cases.cs
+++ Core/Cases.cs
@@ -16,8 +16,8 @@ namespace Core
         public DateOnly updated_at { get; set; }
         public DateOnly created_at { get; set; }
         public int? user_id { get; set; }
-        public string? department_name { get; set; }
-        public string? type { get; set; }
+        public int? department_id { get; set; }
+        public int? type_id { get; set; }
         public int order_item_id { get; set; }
     }
 }

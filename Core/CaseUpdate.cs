f7e0a10bed9667de073ef1211f84b966b76ea5a4
FreddyMail <frederik@coldscale.io>
2026-05-18 11:35:14 +0200

    Added commenting

--- Core/CaseUpdate.cs
+++ Core/CaseUpdate.cs
@@ -3,7 +3,7 @@ namespace Core;
 public class CaseUpdate
 {
     public required string message { get; set; }
-    public required string timestamp { get; set; }
+    public required DateTime timestamp { get; set; }
     public bool isComment { get; set; } = false;
     public string? commentMessage { get; set; }
 }

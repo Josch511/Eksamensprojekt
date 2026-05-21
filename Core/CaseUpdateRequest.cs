7e831f5446ce3f4898a47239219b12273c57a815
FreddyMail <frederik@coldscale.io>
2026-05-21 13:28:23 +0200

    Created CaseDetail page

--- Core/CaseUpdateRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace Core;
-
-public class CaseUpdateRequest
-{
-    public required string message { get; set; }
-    public bool isComment { get; set; } = false;
-    public string? commentMessage { get; set; }
-}
